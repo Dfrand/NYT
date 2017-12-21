b4988a085b1cc3637bf01427f39f5e44ef995133
derek <d.frand@yahoo.com>
2017-12-20 20:51:01 -0600

    added APIkey

---
+++ Assets/Javascript/app.js
@@ -0,0 +1,13 @@
+
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=4a142327a02841f28aa4c1a4044054ea";
+
+
+// AJAX call
+$.ajax({
+	url: queryURL,
+	method: "GET"
+})
+
+.done(function(response){
+	console.log(response);
+})
